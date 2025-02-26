61126046dbf7dbadff6d12f7745359603ddf25d7
Quietonset <davidakachi9@gmail.com>
2025-02-26 18:13:47 +0100

    created react app and added tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-pink-500 text-2xl'>Hello AirStars!</h1>
+      {/* <Button colorScheme={'normal'}>Hello</Button> */}
+    </div>
+  )
+}
+
+export default App
